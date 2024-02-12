149a80a0eda68b4626a7f1446c31357bd5791137
Luke Bayas <lbayas@gmail.com>
2024-02-12 16:14:13 -0500

    set eager to true

--- vllm/config.py
+++ vllm/config.py
@@ -73,7 +73,7 @@ class ModelConfig:
         tokenizer_revision: Optional[str] = None,
         max_model_len: Optional[int] = None,
         quantization: Optional[str] = None,
-        enforce_eager: bool = False,
+        enforce_eager: bool = True,
         max_context_len_to_capture: Optional[int] = None,
     ) -> None:
         self.model = model
